17f251f29ce39cf00ce6e9a094aa81f29ad65f95
JairHJ <jair.herrrera@gmail.com>
2025-08-13 16:19:06 -0600

    Ajustes de AuthServices v9

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -3,6 +3,9 @@ import { HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: any = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const authService = inject(AuthService); // Inyecta AuthService
@@ -18,5 +21,13 @@ export const authInterceptor: any = (req: HttpRequest<unknown>, next: HttpHandle
     });
   }
 
-  return next(authReq);
+  return next(authReq).pipe(
+    catchError((err: any) => {
+      if (err instanceof HttpErrorResponse && err.status === 429) {
+        // Mostrar un mensaje simple (alert) o podrías integrar un ToastService
+        alert('Se superó el límite de peticiones permitidas. Intenta de nuevo más tarde.');
+      }
+      return throwError(() => err);
+    })
+  );
 };
